f1f1fa46b6cc8f277ff4933b5aa582716244773b
Martin Löper <martin.loeper@nesto-software.de>
2021-12-20 20:32:47 +0100

    feat: second parser for keyboard strokes

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const SerialPort = require('serialport')
 const Delimiter = require('@serialport/parser-delimiter')
 const port = new SerialPort('/dev/ttyACM0')
 const open = require('open');
+var ks = require('node-key-sender');
 
 // e.g. https://app.partbolt.com/s/adjustStock/xxxxxxxx?t=r&q=2&c=test#
-const parser = port.pipe(new Delimiter({ delimiter: '#' }))
-parser.on('data', async (data) => {
+const firefoxParser = port.pipe(new Delimiter({ delimiter: '#' }))
+firefoxParser.on('data', async (data) => {
     await open(data.toString(), {
         app: {
             name: open.apps.firefox
         }
     });
+});
+
+const keyboardParser = port.pipe(new Delimiter({ delimiter: '\\' }))
+keyboardParser.on('data', async (data) => {
+    ks.sendText(data);
 });
