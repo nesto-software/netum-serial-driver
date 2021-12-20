d7ae4f9e3cf09673a6afee84ae0fe30696cfda21
Martin Löper <martin.loeper@nesto-software.de>
2021-12-20 20:57:35 +0100

    fix: parsers must apply exclusively

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ var ks = require('node-key-sender');
 // e.g. https://app.partbolt.com/s/adjustStock/xxxxxxxx?t=r&q=2&c=test#
 const firefoxParser = port.pipe(new Delimiter({ delimiter: '#' }))
 firefoxParser.on('data', async (data) => {
-    await open(data.toString(), {
-        app: {
-            name: open.apps.firefox
-        }
-    });
+    if (!data.toString().includes("$")) {
+        await open(data.toString(), {
+            app: {
+                name: open.apps.firefox
+            }
+        });
+    }
 });
 
 const keyboardParser = port.pipe(new Delimiter({ delimiter: '$' }))
 keyboardParser.on('data', async (data) => {
-    ks.sendText(data.toString());
-    ks.sendKey("enter");
+    if (!data.toString().includes("#")) {
+        ks.sendText(data.toString());
+        ks.sendKey("enter");
+    }
 });
 
-port.on('data', async (data) => {
-    console.log(data.toString());
-});
+// uncomment to debug
+//port.on('data', async (data) => {
+//    console.log(data.toString());
+//});
